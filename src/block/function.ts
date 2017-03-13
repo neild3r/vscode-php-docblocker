8275509a4bdb5d0017011866668306d096e0be1e
Neil Brayfield <neil@d3r.com>
2017-03-13 02:44:24 +0000

    FIx issue with function no params

--- src/block/function.ts
+++ src/block/function.ts
@@ -8,13 +8,15 @@ export default class Function extends Block {
     parse():Doc {
         let params = this.match();
 
-        let doc = new Doc();
-        let args = params[5].split(',');
+        let doc = new Doc('Undocumented function');
 
-        for (let index = 0; index < args.length; index++) {
-            let arg = args[index];
-            let parts = arg.match(/^\s*([A-Za-z0-9_]+)?\s*(\$[A-Za-z0-9_]+)\s*\=?\s*(.*)$/);
-            doc.params.push(new Param(parts[1] ? parts[1] : '[type]', parts[2]));
+        if (params[5] != "") {
+            let args = params[5].split(',');
+            for (let index = 0; index < args.length; index++) {
+                let arg = args[index];
+                let parts = arg.match(/^\s*([A-Za-z0-9_]+)?\s*(\$[A-Za-z0-9_]+)\s*\=?\s*(.*)$/);
+                doc.params.push(new Param(parts[1] ? parts[1] : '[type]', parts[2]));
+            }
         }
 
         doc.return = 'void';
