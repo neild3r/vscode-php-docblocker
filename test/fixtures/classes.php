325ea620383c9a42a771b3e8dfda0af2d2872553
Neil Brayfield <neil@d3r.com>
2017-03-16 18:45:30 +0000

    Add more class fixtures

--- test/fixtures/classes.php
+++ test/fixtures/classes.php
@@ -6,6 +6,7 @@ class SimpleClass
 
 }
 
+
 ////=> interface
 interface SimpleInterface
 {
@@ -14,6 +15,53 @@ interface SimpleInterface
 
 ////=> trait
 trait SimpleTrait
+{
+
+}
+
+////=> final
+final class FinalClass
+{
+
+}
+
+////=> abstract
+abstract class AbstractClass
+{
+
+}
+
+////=> extends
+class SubClass extends ParentClass
+{
+
+}
+
+////=> implements
+class ImplementedClass implements ClassInterface
+{
+
+}
+
+////=> extends-implements
+class ImplementedSubClass extends ImplementedParentClass implements ClassInterface
+{
+
+}
+
+////=> multiline
+abstract class Multiline implements
+    ImplementedClass1,
+    ImplementedClass2,
+    ImplementedClass3
+{
+
+}
+
+////=> complex
+abstract class Complex extends MultilineParent implements
+    ImplementedClass1,
+    ImplementedClass2
 {
 
 }
