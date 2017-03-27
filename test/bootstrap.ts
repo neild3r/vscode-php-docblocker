a7efe56a3e955f9444ede88e8667a0da1f80d343
Neil Brayfield <neil@d3r.com>
2017-03-27 01:21:24 +0100

    Add remap to coverage reports

--- test/bootstrap.ts
+++ test/bootstrap.ts
@@ -1,17 +1,16 @@
 import * as path from 'path';
 
-
-
 declare var __coverage__:any;
 
 var istanbul = require('istanbul'),
+    remap = require('remap-istanbul/lib/remap'),
+    loadCoverage = require('remap-istanbul/lib/loadCoverage'),
+    writeReport = require('remap-istanbul/lib/writeReport'),
+    coverageDir = path.resolve(__dirname + '/../../coverage/'),
     hook = istanbul.hook,
     collector = new istanbul.Collector(),
     instrumenter = new istanbul.Instrumenter(),
-    reporter = new istanbul.Reporter(false, __dirname + '/../../coverage/'),
-    sync = true;
-
-
+    reporter = new istanbul.Reporter(false, coverageDir);
 
 hook.hookRequire(function(file) {
     return file.indexOf(path.resolve(__dirname + '/../..') + '/out/src') !== -1;
@@ -20,14 +19,15 @@ hook.hookRequire(function(file) {
     return ret;
 });
 
-// ));
-// __coverage__ = {};
-
-
-process.on('exit', () => {
+export function callback() {
     collector.add(__coverage__);
-    reporter.add('lcovonly');
-    reporter.write(collector, sync, function () {
-        console.log('All reports generated');
+    reporter.addAll(['json']);
+
+
+    reporter.write(collector, false, function () {
+        let collect = remap(loadCoverage(coverageDir + '/coverage-final.json'));
+        writeReport(collect, 'lcovonly', {}, coverageDir + '/lcov.info').then(function () {
+            console.log('Coverage report generated');
+        });
     });
-});
+}
