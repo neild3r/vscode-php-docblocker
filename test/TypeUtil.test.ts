bb99e3d5336f65f8b02bd3b6de16f67eba708a0c
jens1o <jenshausdorf@gmail.com>
2017-05-23 19:19:04 +0200

    add tests for TypeUtil

---
+++ test/TypeUtil.test.ts
@@ -0,0 +1,31 @@
+import * as assert from 'assert';
+import TypeUtil from "../src/util/TypeUtil";
+import { workspace } from "vscode";
+
+suite("TypeUtil tests", () => {
+
+    test("With default settings the integer type formatted is integer", () => {
+        workspace.getConfiguration().update('php-docblocker.useShortNames', false);
+        assert.equal(TypeUtil.getFormattedTypeByName('int'), 'integer');
+    });
+
+    test("With default settings the boolean type formatted is bool", () => {
+        workspace.getConfiguration().update('php-docblocker.useShortNames', false);
+        assert.equal(TypeUtil.getFormattedTypeByName('bool'), 'boolean');
+    });
+
+    test('With special settings the integer type formatted is int', () => {
+        workspace.getConfiguration().update('php-docblocker.useShortNames', true);
+        assert.equal(TypeUtil.getFormattedTypeByName('int'), 'int');
+    })
+
+    test('With special settings the boolean type formatted is bool', () => {
+        workspace.getConfiguration().update('php-docblocker.useShortNames', true);
+        assert.equal(TypeUtil.getFormattedTypeByName('bool'), 'bool');
+    })
+
+    test("Unknown types won't be touched", () => {
+        assert.equal(TypeUtil.getFormattedTypeByName('helloWorld'), 'helloWorld');
+    });
+
+});
