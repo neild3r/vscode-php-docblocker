5effec488cd131a09d6edbb822df83b6d59e14d9
Michael Alexander <beefsack@gmail.com>
2017-07-05 12:31:33 +1000

    Added HHVM/Hack support

--- src/extension.ts
+++ src/extension.ts
@@ -11,31 +11,33 @@ import Completions from "./completions";
  */
 export function activate(context: vscode.ExtensionContext)
 {
-    vscode.languages.setLanguageConfiguration('php', {
-        wordPattern: /(-?\d*\.\d\w*)|([^\-\`\~\!\@\#\%\^\&\*\(\)\=\+\[\{\]\}\\\|\;\:\'\"\,\.\<\>\/\?\s]+)/g,
-        onEnterRules: [
-            {
-                // e.g. /** | */
-                beforeText: /^\s*\/\*\*(?!\/)([^\*]|\*(?!\/))*$/,
-                afterText: /^\s*\*\/$/,
-                action: { indentAction: vscode.IndentAction.IndentOutdent, appendText: ' * ' }
-            }, {
-                // e.g. /** ...|
-                beforeText: /^\s*\/\*\*(?!\/)([^\*]|\*(?!\/))*$/,
-                action: { indentAction: vscode.IndentAction.None, appendText: ' * ' }
-            }, {
-                // e.g.  * ...|
-                beforeText: /^(\t|(\ \ ))*\ \*(\ ([^\*]|\*(?!\/))*)?$/,
-                action: { indentAction: vscode.IndentAction.None, appendText: '* ' }
-            }, {
-                // e.g.  */|
-                beforeText: /^(\t|(\ \ ))*\ \*\/\s*$/,
-                action: { indentAction: vscode.IndentAction.None, removeText: 1 }
-            }
-        ]
-    });
+    ['php', 'hack'].forEach(lang => {
+        vscode.languages.setLanguageConfiguration(lang, {
+            wordPattern: /(-?\d*\.\d\w*)|([^\-\`\~\!\@\#\%\^\&\*\(\)\=\+\[\{\]\}\\\|\;\:\'\"\,\.\<\>\/\?\s]+)/g,
+            onEnterRules: [
+                {
+                    // e.g. /** | */
+                    beforeText: /^\s*\/\*\*(?!\/)([^\*]|\*(?!\/))*$/,
+                    afterText: /^\s*\*\/$/,
+                    action: { indentAction: vscode.IndentAction.IndentOutdent, appendText: ' * ' }
+                }, {
+                    // e.g. /** ...|
+                    beforeText: /^\s*\/\*\*(?!\/)([^\*]|\*(?!\/))*$/,
+                    action: { indentAction: vscode.IndentAction.None, appendText: ' * ' }
+                }, {
+                    // e.g.  * ...|
+                    beforeText: /^(\t|(\ \ ))*\ \*(\ ([^\*]|\*(?!\/))*)?$/,
+                    action: { indentAction: vscode.IndentAction.None, appendText: '* ' }
+                }, {
+                    // e.g.  */|
+                    beforeText: /^(\t|(\ \ ))*\ \*\/\s*$/,
+                    action: { indentAction: vscode.IndentAction.None, removeText: 1 }
+                }
+            ]
+        });
 
-    vscode.languages.registerCompletionItemProvider('php', new Completions(), '*', '@');
+        vscode.languages.registerCompletionItemProvider(lang, new Completions(), '*', '@');
+    });
 }
 
 /**
