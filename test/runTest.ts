a9b900aca2b5ff51532bb2dbf05828e5f39d59b7
Neil Brayfield <neil@d3r.com>
2019-11-14 16:19:28 +0000

    Download vscode seperately for debugging

--- test/runTest.ts
+++ test/runTest.ts
@@ -1,24 +1,30 @@
 import * as path from 'path';
 
-import { runTests } from 'vscode-test';
+import { runTests,downloadAndUnzipVSCode } from 'vscode-test';
 
 async function main() {
-	try {
-		// The folder containing the Extension Manifest package.json
-		// Passed to `--extensionDevelopmentPath`
-		const extensionDevelopmentPath = path.resolve(__dirname, '../../../');
+    try {
+        var vscodeExecutablePath = await downloadAndUnzipVSCode('1.35.0')
+    } catch (err) {
+        console.error(err);
+    }
 
-		// The path to the extension test script
-		// Passed to --extensionTestsPath
-		const extensionTestsPath = path.resolve(__dirname, './index');
+    try {
+        // The folder containing the Extension Manifest package.json
+        // Passed to `--extensionDevelopmentPath`
+        const extensionDevelopmentPath = path.resolve(__dirname, '../../../');
 
-		// Download VS Code, unzip it and run the integration test
-		await runTests({ extensionDevelopmentPath, extensionTestsPath });
-	} catch (err) {
-		console.error('Failed to run tests');
-		console.error(err);
-		process.exit(1);
-	}
+        // The path to the extension test script
+        // Passed to --extensionTestsPath
+        const extensionTestsPath = path.resolve(__dirname, './index');
+
+        // Download VS Code, unzip it and run the integration test
+        await runTests({ vscodeExecutablePath, extensionDevelopmentPath, extensionTestsPath });
+    } catch (err) {
+        console.error('Failed to run tests');
+        console.error(err);
+        process.exit(1);
+    }
 }
 
 main();
