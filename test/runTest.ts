513543f48df8bc300e93d601a0c32327bdf5b6a1
Neil Brayfield <neil@d3r.com>
2019-11-15 15:41:42 +0000

    Change back to compiling on latest vscode

--- test/runTest.ts
+++ test/runTest.ts
@@ -4,12 +4,6 @@ import { runTests,downloadAndUnzipVSCode } from 'vscode-test';
 
 async function main() {
     try {
-        var vscodeExecutablePath = await downloadAndUnzipVSCode('1.35.0')
-    } catch (err) {
-        console.error(err);
-    }
-
-    try {
         // The folder containing the Extension Manifest package.json
         // Passed to `--extensionDevelopmentPath`
         const extensionDevelopmentPath = path.resolve(__dirname, '../../../');
@@ -19,10 +13,9 @@ async function main() {
         const extensionTestsPath = path.resolve(__dirname, './index');
 
         // Download VS Code, unzip it and run the integration test
-        await runTests({ vscodeExecutablePath, extensionDevelopmentPath, extensionTestsPath });
+        await runTests({ extensionDevelopmentPath, extensionTestsPath });
     } catch (err) {
         console.error('Failed to run tests');
-        console.error(err);
         process.exit(1);
     }
 }
