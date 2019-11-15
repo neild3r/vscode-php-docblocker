03a4ffc0013d2796d81dd4c5aca45d27cfc9683b
Neil Brayfield <neil@d3r.com>
2019-11-15 17:41:41 +0000

    Add code coverage back in

--- test/index.ts
+++ test/index.ts
@@ -2,7 +2,23 @@ import * as path from 'path';
 import * as Mocha from 'mocha';
 import * as glob from 'glob';
 
-export function run(): Promise<void> {
+export async function run(): Promise<void> {
+    const NYC = require('nyc');
+    const nyc = new NYC({
+      cwd: path.join(__dirname, '..', '..'),
+      exclude: ['**/test/**', '.vscode-test/**'],
+      reporter: ['text', 'html'],
+      all: true,
+      instrument: true,
+      hookRequire: true,
+      hookRunInContext: true,
+      hookRunInThisContext: true,
+    });
+
+    nyc.reset();
+    nyc.wrap();
+
+
     // Create the mocha test
     const mocha = new Mocha({
         ui: 'tdd',
@@ -11,27 +27,33 @@ export function run(): Promise<void> {
 
     const testsRoot = path.resolve(__dirname, '..');
 
-    return new Promise((c, e) => {
-        glob('**/**.test.js', { cwd: testsRoot }, (err, files) => {
-            if (err) {
-                return e(err);
-            }
+    try {
+        await new Promise((c, e) => {
+            glob('**/**.test.js', { cwd: testsRoot }, (err, files) => {
+                if (err) {
+                    return e(err);
+                }
 
-            // Add files to the test suite
-            files.forEach(f => mocha.addFile(path.resolve(testsRoot, f)));
+                // Add files to the test suite
+                files.forEach(f => mocha.addFile(path.resolve(testsRoot, f)));
 
-            try {
-                // Run the mocha test
-                mocha.run(failures => {
-                    if (failures > 0) {
-                        e(new Error(`${failures} tests failed.`));
-                    } else {
-                        c();
-                    }
-                });
-            } catch (err) {
-                e(err);
-            }
+                try {
+                    // Run the mocha test
+                    mocha.run(failures => {
+                        if (failures > 0) {
+                            e(new Error(`${failures} tests failed.`));
+                        } else {
+                            c();
+                        }
+                    });
+                } catch (err) {
+                    e(err);
+                }
+            });
         });
-    });
+    } finally {
+        nyc.writeCoverageFile();
+        nyc.report();
+        console.log(nyc);
+    }
 }
