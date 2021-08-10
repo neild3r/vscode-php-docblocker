dce282fb486e86aabba6468478d16caddaa32d12
Neil Brayfield <neil@d3r.com>
2021-08-10 08:50:23 +0100

    Bundle with esbuild

--- test/index.ts
+++ test/index.ts
@@ -43,7 +43,7 @@ export async function run(): Promise<void> {
                         if (failures > 0) {
                             e(new Error(`${failures} tests failed.`));
                         } else {
-                            c();
+                            c(failures);
                         }
                     });
                 } catch (err) {
